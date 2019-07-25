a0fa59d7995f1fc9720a7b9c9ff4d7aad506522d
T.J <t.ruggles@live.ca>
2019-07-25 17:50:46 -0400

    Setting up NavLinks between pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom"
 
 import ExpenseDashboardPage from "./pages/ExpenseDashboardPage"
 
@@ -10,17 +10,42 @@ import "./styles/styles.scss"
 const AddExpensePage = () => <div>This is from add expense page</div>
 const EditExpensePage = () => <div>This is from edit expense page</div>
 const HelpPage = () => <div>This is from help page</div>
-const NotFoundPage = () => <div>404</div>
+const NotFoundPage = () => (
+    <div>
+        404 = <Link to="/">Go Home</Link>
+    </div>
+)
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink exact to="/" activeClassName="isActive">
+            Dashboard
+        </NavLink>
+        <NavLink to="/create" activeClassName="isActive">
+            Add Expense
+        </NavLink>
+        <NavLink to="/edit" activeClassName="isActive">
+            Edit Expense
+        </NavLink>
+        <NavLink to="/help" activeClassName="isActive">
+            Help
+        </NavLink>
+    </header>
+)
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={ExpenseDashboardPage} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={ExpenseDashboardPage} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 
