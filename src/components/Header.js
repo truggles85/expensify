0f18512aff4116d9e8f6f302ae5d1096a21bdfb5
T.J <t.ruggles@live.ca>
2019-07-25 18:27:54 -0400

    Setting up edit url to be dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,6 @@ const Header = () => (
         <NavLink to="/create" activeClassName="isActive">
             Add Expense
         </NavLink>
-        <NavLink to="/edit" activeClassName="isActive">
-            Edit Expense
-        </NavLink>
         <NavLink to="/help" activeClassName="isActive">
             Help
         </NavLink>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,7 +15,7 @@ const AppRouter = () => (
             <Switch>
                 <Route exact path="/" component={ExpenseDashboardPage} />
                 <Route path="/create" component={CreateExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
