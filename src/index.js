6669406da7a9c49fbafed8aff7be3ab30525e4bc
T.J <t.ruggles@live.ca>
2019-07-30 17:20:24 -0400

    Refactoring using actions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 // import AppRouter from "./routers/AppRouter"
-// import Redux from "./playground/redux-101"
-import Destructuring from "./playground/destructuring"
+import Redux from "./playground/redux-101"
+// import Destructuring from "./playground/destructuring"
 
 import "normalize.css"
 import "./styles/styles.scss"
 
-ReactDOM.render(<Destructuring />, document.getElementById("root"))
+ReactDOM.render(<Redux />, document.getElementById("root"))
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,19 +2,34 @@ import React from "react"
 import { createStore } from "redux"
 
 const Redux = () => {
+    const incrementCount = ({ incrementBy = 1 } = {}) => ({
+        type: "INCREMENT",
+        incrementBy,
+    })
+
+    const decrementCount = ({ decrementBy = 1 } = {}) => ({
+        type: "DECREMENT",
+        decrementBy,
+    })
+
+    const setCount = ({ count }) => ({
+        type: "SET",
+        count,
+    })
+
+    const resetCount = () => ({
+        type: "RESET",
+    })
+
     const store = createStore((state = { count: 0 }, action) => {
-        const incrementBy =
-            typeof action.incrementBy === "number" ? action.incrementBy : 1
-        const decrementBy =
-            typeof action.decrementBy === "number" ? action.decrementBy : 1
         switch (action.type) {
             case "INCREMENT":
                 return {
-                    count: state.count + incrementBy,
+                    count: state.count + action.incrementBy,
                 }
             case "DECREMENT":
                 return {
-                    count: state.count - decrementBy,
+                    count: state.count - action.decrementBy,
                 }
             case "SET":
                 return {
@@ -33,32 +48,17 @@ const Redux = () => {
         console.log(store.getState())
     })
 
-    store.dispatch({
-        type: "INCREMENT",
-        incrementBy: 5,
-    })
+    store.dispatch(incrementCount({ incrementBy: 5 }))
 
-    store.dispatch({
-        type: "INCREMENT",
-    })
+    store.dispatch(incrementCount())
 
-    store.dispatch({
-        type: "RESET",
-    })
+    store.dispatch(resetCount())
 
-    store.dispatch({
-        type: "DECREMENT",
-    })
+    store.dispatch(decrementCount())
 
-    store.dispatch({
-        type: "DECREMENT",
-        decrementBy: 10,
-    })
+    store.dispatch(decrementCount({ decrementBy: 10 }))
 
-    store.dispatch({
-        type: "SET",
-        count: 101,
-    })
+    store.dispatch(setCount({ count: 101 }))
 
     return <div>Redux app</div>
 }
