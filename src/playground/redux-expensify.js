e92738b2bfec61e57797fb6bf30e94e003fa1aa1
T.J <t.ruggles@live.ca>
2019-08-02 02:05:08 -0400

    Creating editExpense and setTextFilter actions

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -25,8 +25,20 @@ const ReduxExpensify = () => {
         type: "REMOVE_EXPENSE",
         id,
     })
+
     // EDIT_EXPENSE
+    const editExpense = (id, updates) => ({
+        type: "EDIT_EXPENSE",
+        id,
+        updates,
+    })
+
     // SET_TEXT_FILTER
+    const setTextFilter = (text = "") => ({
+        type: "SET_TEXT_FILTER",
+        text,
+    })
+
     // SORT_BY_DATE
     // SORT_BY_AMOUNT
     // SET_START_DATE
@@ -39,6 +51,17 @@ const ReduxExpensify = () => {
                 return [...state, action.expense]
             case "REMOVE_EXPENSE":
                 return state.filter(({ id }) => id !== action.id)
+            case "EDIT_EXPENSE":
+                return state.map((expense) => {
+                    if (expense.id === action.id) {
+                        return {
+                            ...expense,
+                            ...action.updates,
+                        }
+                    } else {
+                        return expense
+                    }
+                })
             default:
                 return state
         }
@@ -54,6 +77,11 @@ const ReduxExpensify = () => {
 
     const filtersReducer = (state = filtersReducerDefaultState, action) => {
         switch (action.type) {
+            case "SET_TEXT_FILTER":
+                return {
+                    ...state,
+                    text: action.text,
+                }
             default:
                 return state
         }
@@ -85,6 +113,11 @@ const ReduxExpensify = () => {
     )
 
     store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+
+    store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+
+    store.dispatch(setTextFilter("rent"))
+    store.dispatch(setTextFilter())
 
     const demoState = {
         expenses: [
