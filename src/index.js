731047e5f0d4de44ddb92d93d0b90d11d7cba5cf
T.J <t.ruggles@live.ca>
2019-08-04 19:49:21 -0400

    Hooking up React with Redux and adding ExpenseList and ExpenseListItem components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
 
 import AppRouter from "./routers/AppRouter"
 import configureStore from "./store/configureStore"
@@ -15,19 +16,31 @@ const store = configureStore()
 store.dispatch(
     addExpense({
         description: "water bill",
+        amount: "4500",
     })
 )
 
 store.dispatch(
     addExpense({
         description: "gas bill",
+        amount: "50",
     })
 )
 
 store.dispatch(setTextFilter("water"))
 
+setTimeout(() => {
+    store.dispatch(setTextFilter("bill"))
+}, 3000)
+
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"))
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById("root"))
--- src/pages/ExpenseDashboardPage.js
+++ src/pages/ExpenseDashboardPage.js
@@ -1,7 +1,12 @@
 import React from "react"
+import ExpenseList from "../components/ExpenseList"
 
 function ExpenseDashboardPage() {
-    return <div>This is from ExpenseDashboard page</div>
+    return (
+        <div>
+            <ExpenseList />
+        </div>
+    )
 }
 
 export default ExpenseDashboardPage
